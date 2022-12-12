860c7c9a740400b019b13b17cedd9a8a372aca6d
Matthieu Laroche <matthieu.laroche@telecom-bretagne.eu>
2022-12-12 16:53:37 +0100

    v0.9

--- docker/bin/start.sh
+++ docker/bin/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/app/bin/wait-for-it.sh $INFLUXDB_URL -- java -cp /usr/app/bin/vertigo-analytics-server-0.8-jar-with-dependencies.jar -Djava.awt.headless=true io.vertigo.analytics.server.AnalyticsServerStarter log4j2 4562 /usr/app/conf/log4j2.xml
+/usr/app/bin/wait-for-it.sh $INFLUXDB_URL -- java -cp /usr/app/bin/vertigo-analytics-server-0.9-jar-with-dependencies.jar -Djava.awt.headless=true io.vertigo.analytics.server.AnalyticsServerStarter log4j2 4562 /usr/app/conf/log4j2.xml
