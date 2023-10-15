69d35699430ad22e313d82922ee16b87b5ed8159
Piedeloup Nicolas <npi2loup@gmail.com>
2023-10-15 20:25:04 +0200

    default start legacy serialized 4562, json-gz 4563

--- docker/bin/start.sh
+++ docker/bin/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/app/bin/wait-for-it.sh $INFLUXDB_URL -- java -cp /usr/app/bin/vertigo-analytics-server-0.13.0-jar-with-dependencies.jar -Djava.awt.headless=true io.vertigo.analytics.server.AnalyticsServerStarter log4j2-gz 4562 /usr/app/conf/log4j2.xml log4j2json 4563 /usr/app/conf/log4j2.xml
+/usr/app/bin/wait-for-it.sh $INFLUXDB_URL -- java -cp /usr/app/bin/vertigo-analytics-server-0.13.0-jar-with-dependencies.jar -Djava.awt.headless=true io.vertigo.analytics.server.AnalyticsServerStarter log4j2 4562 /usr/app/conf/log4j2.xml log4j2json-gz 4563 /usr/app/conf/log4j2.xml
