0a66bae10f01365a140bf97e20806fb8099a166b
Piedeloup Nicolas <npi2loup@gmail.com>
2024-03-08 14:54:37 +0100

    prepare 0.14.0

--- docker/bin/start.sh
+++ docker/bin/start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/app/bin/wait-for-it.sh $INFLUXDB_URL -- java -cp /usr/app/bin/vertigo-analytics-server-0.13.1-jar-with-dependencies.jar -Djava.awt.headless=true io.vertigo.analytics.server.AnalyticsServerStarter log4j2 4562 /usr/app/conf/log4j2.xml log4j2json-gz 4563 /usr/app/conf/log4j2.xml
+/usr/app/bin/wait-for-it.sh $INFLUXDB_URL -- java -cp /usr/app/bin/vertigo-analytics-server-0.14.0-jar-with-dependencies.jar -Djava.awt.headless=true io.vertigo.analytics.server.AnalyticsServerStarter log4j2 4562 /usr/app/conf/log4j2.xml log4j2json-gz 4563 /usr/app/conf/log4j2.xml
